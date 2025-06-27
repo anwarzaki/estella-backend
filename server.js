956ddd4d86ab9bbe897db4def9b44d67d4d657c0
anwarzaki <anwarzaki856@gmail.com>
2025-06-27 18:05:44 +0530

    remove /

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5000','https://estella-paris-perfume.vercel.app/'], 
+  origin: ['http://localhost:5173', 'http://localhost:5000','https://estella-paris-perfume.vercel.app'], 
   credentials: true
 }));
 
